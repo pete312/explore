d408a56564510c58c6fab831cbbc429825cc37ba
Pete Moore <1petemoore@gmail.com>
2018-05-06 23:59:05 -0500

    added lib for logger

---
+++ testPython/lib/logger_test_module.py
@@ -0,0 +1,22 @@
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+def funcA(message):
+    logger = logging.getLogger(__name__ + ".funcA")
+    logger.info("at funcA \"%s\"", message)
+    logger.warn("at funcA \"%s\"", message)
+    logger.error("at funcA \"%s\"", message)
+    
+    
+def fail():
+    logger.warn("warn")
+    logger.exception("this is it")
+
+
+
+
+
+
