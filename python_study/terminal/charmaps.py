10d02a4863c78304aa5f9be11a3fbf4b6250117a
Pete Moore <1petemoore@gmail.com>
2018-10-14 16:32:55 -0500

    refined char set

--- python_study/terminal/charmaps.py
+++ python_study/terminal/charmaps.py
@@ -3,6 +3,7 @@
 from colored import fg, attr
 
 print('take a look at gucharmap')
+print('and https://www.pythonsheets.com/notes/python-unicode.html')
 
 print("interesting range of characters")
 #for c in range(int(0xff), int(0xd700)): 
@@ -10,15 +11,16 @@ print("interesting range of characters")
 
 
 def cprint(start, finish, i=[1]):
-    print("\n\n---%x--%x-------------------------------------------------------------------------\n" % (start,finish))
+    print("\n\n---range(%x - %x)------------------------------------------------\n" % (start,finish))
     for c in range(int(start), int(finish)):
         print('%s %s' % (fg(i[0]), chr(c)),end='')
         i[0] = i[0] % 200 + 1
+    print(attr(0))
 
 i = 1
-cprint(0x2400,0x2900,[i])
+cprint(0x2500,0x2900,[i])
 cprint(0x2ff0,0x2ffe,[i])
-cprint(0x2400,0x2ffe,[i])
+cprint(0x2d80,0x2e00,[i])
 cprint(0x2500,0x26b5,[i])
 cprint(0x2d30,0x2d66,[i])
 
