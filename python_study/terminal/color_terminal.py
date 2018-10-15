c9090834c7af9f4a6fd24b9848dd44fca3401465
Pete Moore <1petemoore@gmail.com>
2018-10-14 23:20:37 -0500

    tweak

--- python_study/terminal/color_terminal.py
+++ python_study/terminal/color_terminal.py
@@ -6,11 +6,11 @@ import colored
 print('https://pypi.org/project/colored/')
 
 for i in range(1,255):  
-    print('%s color test %s' %  ( colored.fg(i), colored.attr(0) ))
+    print('%s color test %s%s' %  ( colored.fg(i), colored.attr(0),i ))
     
 input("enter to continue")
 
 for i in range(1,255):  
-    print('%s color test %s' %  ( colored.fg(i), colored.attr(0) ))
+    #print('%s color test %s' %  ( colored.fg(i), colored.attr(0),i ))
     for ii in range(1,255):
-        print('%s%s color test %s' %  ( colored.fg(i), colored.bg(ii), colored.attr(0) ))
+        print('%s%s color test %s %s %s' %  ( colored.fg(i), colored.bg(ii), colored.attr(0),i,ii ))
